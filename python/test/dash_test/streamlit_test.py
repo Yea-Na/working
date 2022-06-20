b5987347acf74f508d46505ae79caf491574b8ac
yea-na <jenna.shin@dcuration.co.kr>
2022-06-20 15:28:05 +0900

    test2

--- python/test/dash_test/streamlit_test.py
+++ python/test/dash_test/streamlit_test.py
@@ -14,7 +14,7 @@ st.set_page_config(layout="wide")
 
 @st.cache
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+    data = pd.read_csv(DATA_URL, nrows=nrows) 
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
